d3851bf08e718e1cccac3ad640b40491dd8feaa6
Alan <Alan@Alan-PC>
2012-04-30 23:23:31 -0400

    Added dirt tilling support

--- NoItem/src/net/worldoftomorrow/nala/ni/ToolListener.java
+++ NoItem/src/net/worldoftomorrow/nala/ni/ToolListener.java
@@ -1,10 +1,14 @@
 package net.worldoftomorrow.nala.ni;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ToolListener implements Listener{
 	
@@ -23,6 +27,25 @@ public class ToolListener implements Listener{
 			event.setCancelled(true);
 			if(Configuration.notifyNoUse()){
 				p.sendMessage(ChatColor.BLUE + Configuration.noUseMessage());
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onLandFarm(PlayerInteractEvent event){
+		Block block = event.getClickedBlock();
+		if(event.getAction() == Action.RIGHT_CLICK_BLOCK){
+			if(block.getType() == Material.DIRT || block.getType() == Material.GRASS){
+				int inHand = event.getPlayer().getItemInHand().getTypeId();
+				Player p = event.getPlayer();
+				if(inHand >= 290 && inHand <= 294){
+					if(Permissions.NOUSE.has(p, inHand) && !p.isOp() && !Permissions.ALLITEMS.has(p)){
+						event.setCancelled(true);
+						if(Configuration.notifyNoUse()){
+							p.sendMessage(ChatColor.BLUE + Configuration.noUseMessage());
+						}
+					}
+				}
 			}
 		}
 	}
