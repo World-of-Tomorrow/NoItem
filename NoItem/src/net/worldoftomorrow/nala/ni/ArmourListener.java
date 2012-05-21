dee7eae255da7f39b7b735056222cba84ad6c814
Alan <Alan@Alan-PC>
2012-05-21 17:56:33 -0400

    Remove unused import.

--- NoItem/src/net/worldoftomorrow/nala/ni/ArmourListener.java
+++ NoItem/src/net/worldoftomorrow/nala/ni/ArmourListener.java
@@ -8,7 +8,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.InventoryType.SlotType;
-import org.bukkit.inventory.ItemStack;
 
 public class ArmourListener implements Listener {
 	
