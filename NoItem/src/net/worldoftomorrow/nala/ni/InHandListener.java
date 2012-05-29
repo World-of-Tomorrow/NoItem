88f412ba4cfed7d87b357278e23493d3b0070ad3
Alan <Alan@Alan-PC>
2012-05-29 16:38:06 -0400

    Improve the way No Hold handles items being placed in the quickbar

--- NoItem/src/net/worldoftomorrow/nala/ni/InHandListener.java
+++ NoItem/src/net/worldoftomorrow/nala/ni/InHandListener.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
@@ -23,13 +22,11 @@ public class InHandListener implements Listener{
 		int ns = event.getNewSlot();
 		int ps = event.getPreviousSlot();
 		int iid = 0;
-		//int dv = 0;
 		ItemStack notAllowed = null;
 		ItemStack allowed = null;
 		
 		if(p.getInventory().getItem(ns) != null){
 			iid = p.getInventory().getItem(ns).getTypeId();
-			//dv = p.getInventory().getItem(ns).getDurability();
 			notAllowed = p.getInventory().getItem(ns);
 		}
 		if(p.getInventory().getItem(ps) != null){
@@ -50,8 +47,9 @@ public class InHandListener implements Listener{
 	
 	@EventHandler
 	public void onItemInvPlace(InventoryClickEvent event){
-		if(event.getSlotType().equals(SlotType.QUICKBAR)){
-			Player p = Bukkit.getPlayer(event.getWhoClicked().getName());
+		Player p = Bukkit.getPlayer(event.getWhoClicked().getName());
+		//If the slot is selected that is trying to be used
+		if(p.getInventory().getHeldItemSlot() == event.getSlot()){
 			int iid = 0;
 			if(event.getCursor() != null){
 				iid = event.getCursor().getTypeId();
