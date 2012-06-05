f25fa65e92aeb403ab9f313c6435c6fb7a3e1656
nala3 <alanjlitz@gmail.com>
2012-06-05 06:47:17 -0400

    Improve PickupListener (should now work properly with armor + tools

--- NoItem/src/net/worldoftomorrow/nala/ni/PickupListener.java
+++ NoItem/src/net/worldoftomorrow/nala/ni/PickupListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class PickupListener implements Listener {
 	
@@ -13,14 +14,19 @@ public class PickupListener implements Listener {
 	public void onPickup(PlayerPickupItemEvent event) {
 
 		Player p = event.getPlayer();
+		ItemStack stack = new ItemStack(event.getItem().getItemStack());
 		int iid = event.getItem().getItemStack().getTypeId();
 		
+		if(Tools.isTool(iid) /*TODO: or is armor*/){
+			stack.setDurability((short) 0);
+		}
+		
 		log.debug("PlayerPickupItemEvent fired. ".concat(Integer.toString(iid)));
-		if(Perms.NOPICKUP.has(p, event.getItem().getItemStack())){
+		if(Perms.NOPICKUP.has(p, stack)){
 			event.setCancelled(true);
 			event.getItem().setPickupDelay(200);
 			StringHelper.notifyPlayer(p, EventTypes.PICKUP, iid);
-			StringHelper.notifyAdmin(p, EventTypes.PICKUP, event.getItem().getItemStack());
+			StringHelper.notifyAdmin(p, EventTypes.PICKUP, stack);
 		} else {
 			log.debug("Item can be picked up");
 		}
