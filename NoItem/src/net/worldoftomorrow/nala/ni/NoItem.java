a523b344e1f4102f7d95bc9d9de39036a663dc41
nala3 <alanjlitz@gmail.com>
2012-06-29 04:26:35 -0400

    I should actually turn metrics on!

--- NoItem/src/net/worldoftomorrow/nala/ni/NoItem.java
+++ NoItem/src/net/worldoftomorrow/nala/ni/NoItem.java
@@ -1,86 +1,95 @@
 package net.worldoftomorrow.nala.ni;
 
+import java.io.IOException;
+
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoItem extends JavaPlugin {
 
-    protected Config config;
-    protected Vault vault;
-    private static NoItem plugin;
+	protected Config config;
+	protected Vault vault;
+	private static NoItem plugin;
+	private Metrics metrics;
 
-    public void onEnable() {
-	plugin = this;
-	this.config = new Config(this);
-	this.vault = new Vault(this);
-	CommandListener cl = new CommandListener(this);
-	this.getCommand("noitem").setExecutor(cl);
+	public void onEnable() {
+		plugin = this;
+		this.config = new Config(this);
+		this.vault = new Vault(this);
+		CommandListener cl = new CommandListener(this);
+		this.getCommand("noitem").setExecutor(cl);
 
-	Updater updater = new Updater(this.getDescription().getVersion());
-	if (!updater.isLatest()) {
-	    Log.info("--------------------No Item--------------------");
-	    Log.info("There is a new version ( " + updater.getLatest()
-		    + " ) available.");
-	    Log.info("Download it at: " + this.getDescription().getWebsite());
-	    if (Config.pluginChannel().equalsIgnoreCase("dev")) {
-		Log.info("You are using the development version channel!");
-	    }
-	    Log.info("-----------------------------------------------");
+		Updater updater = new Updater(this.getDescription().getVersion());
+		if (!updater.isLatest()) {
+			Log.info("--------------------No Item--------------------");
+			Log.info("There is a new version ( " + updater.getLatest()
+					+ " ) available.");
+			Log.info("Download it at: " + this.getDescription().getWebsite());
+			if (Config.pluginChannel().equalsIgnoreCase("dev")) {
+				Log.info("You are using the development version channel!");
+			}
+			Log.info("-----------------------------------------------");
+		}
+
+		PluginManager pm = getServer().getPluginManager();
+
+		boolean craftListen = Config.stopCrafting();
+		boolean pickupListen = Config.stopItemPickup();
+		boolean brewListen = Config.stopPotionBrew();
+		boolean toolListen = Config.stopToolUse();
+		boolean holdListen = Config.stopItemHold();
+		boolean wearListen = Config.stopArmourWear();
+		boolean cookListen = Config.stopItemCook();
+		boolean dropListen = Config.stopItemDrop();
+
+		if (craftListen) {
+			pm.registerEvents(new CraftingListener(), this);
+		}
+		if (pickupListen) {
+			pm.registerEvents(new PickupListener(), this);
+		}
+		if (brewListen) {
+			pm.registerEvents(new BrewingListener(), this);
+		}
+		if (toolListen) {
+			pm.registerEvents(new NoUseListener(), this);
+		}
+		if (holdListen) {
+			pm.registerEvents(new HoldListener(), this);
+		}
+		if (wearListen) {
+			pm.registerEvents(new ArmorListener(), this);
+		}
+		if (cookListen) {
+			pm.registerEvents(new FurnaceListener(), this);
+		}
+		if (dropListen) {
+			pm.registerEvents(new DropListener(), this);
+		}
+		Log.info("Configs loaded, events registered, and cake baked.");
+
+		if ((!craftListen) && (!pickupListen) && (!brewListen) && (!toolListen)
+				&& (!holdListen) && (!wearListen) && (!cookListen)) {
+			Log.info("No listeners active! Shutting down plugin.");
+			getPluginLoader().disablePlugin(this);
+		}
+
+		try {
+			metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException ex) {
+		}
 	}
 
-	PluginManager pm = getServer().getPluginManager();
+	public void onDisable() {
+		Log.info("Configs unloaded, events unregisterededed, and cake eaten.");
+	}
 
-	boolean craftListen = Config.stopCrafting();
-	boolean pickupListen = Config.stopItemPickup();
-	boolean brewListen = Config.stopPotionBrew();
-	boolean toolListen = Config.stopToolUse();
-	boolean holdListen = Config.stopItemHold();
-	boolean wearListen = Config.stopArmourWear();
-	boolean cookListen = Config.stopItemCook();
-	boolean dropListen = Config.stopItemDrop();
+	public static NoItem getPlugin() {
+		return plugin;
+	}
 
-	if (craftListen) {
-	    pm.registerEvents(new CraftingListener(), this);
+	public Vault getVault() {
+		return vault;
 	}
-	if (pickupListen) {
-	    pm.registerEvents(new PickupListener(), this);
-	}
-	if (brewListen) {
-	    pm.registerEvents(new BrewingListener(), this);
-	}
-	if (toolListen) {
-	    pm.registerEvents(new NoUseListener(), this);
-	}
-	if (holdListen) {
-	    pm.registerEvents(new HoldListener(), this);
-	}
-	if (wearListen) {
-	    pm.registerEvents(new ArmorListener(), this);
-	}
-	if (cookListen) {
-	    pm.registerEvents(new FurnaceListener(), this);
-	}
-	if (dropListen) {
-	    pm.registerEvents(new DropListener(), this);
-	}
-	Log.info("Configs loaded, events registered, and cake baked.");
-
-	if ((!craftListen) && (!pickupListen) && (!brewListen) && (!toolListen)
-		&& (!holdListen) && (!wearListen) && (!cookListen)) {
-	    Log.info("No listeners active! Shutting down plugin.");
-	    getPluginLoader().disablePlugin(this);
-	}
-    }
-
-    public void onDisable() {
-	Log.info("Configs unloaded, events unregisterededed, and cake eaten.");
-    }
-
-    public static NoItem getPlugin() {
-	return plugin;
-    }
-
-    public Vault getVault() {
-	return vault;
-    }
 }
