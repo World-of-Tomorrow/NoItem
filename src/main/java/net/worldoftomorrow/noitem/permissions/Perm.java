086544cb111e1118c4a0f058b1883062e91ac85b
nala3 <alanjlitz@gmail.com>
2013-02-13 21:32:55 -0500

    Cleanup

--- src/main/java/net/worldoftomorrow/noitem/permissions/Perm.java
+++ src/main/java/net/worldoftomorrow/noitem/permissions/Perm.java
@@ -7,7 +7,7 @@ public class Perm {
 	public static final String CMD_DBG_TOGGLE = "noitem.command.debug.toggle";
 	public static final String CMD_DBG_DUMP = "noitem.command.debug.dump";
 	public static final String CMD_VERSION = "noitem.command.version";
-	public static final String ALLITEMS = "noitem.allitems"; //
+	public static final String ALLITEMS = "noitem.allitems";
 	public static final String ONDEATH = "noitem.ondeath.keep";
 
 	public static final String CRAFT = "noitem.nocraft.";
@@ -22,9 +22,7 @@ public class Perm {
 	public static final String COOK = "noitem.nocook.";
 	public static final String PLACE = "noitem.noplace.";
 	public static final String BREAK = "noitem.nobreak.";
-	// Not actually needed because nointeract.right achieves the same thing
-	@Deprecated
-	public static final String OPEN = "noitem.noopen.";
+
 	public static final String HAVE = "noitem.nohave.";
 	public static final String ENCHANT = "noitem.noenchant.";
 	public static final String USE = "noitem.nouse.";
