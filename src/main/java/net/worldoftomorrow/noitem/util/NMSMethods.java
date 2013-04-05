e2dddb23eca3a2f6de139278bd983b1ad04007ce
Christian Hood <christianhood@gmail.com>
2013-04-05 12:16:16 -0700

    Update for 1.5.1

--- src/main/java/net/worldoftomorrow/noitem/util/NMSMethods.java
+++ src/main/java/net/worldoftomorrow/noitem/util/NMSMethods.java
@@ -1,10 +1,10 @@
 package net.worldoftomorrow.noitem.util;
 
-import org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_5_R2.inventory.CraftItemStack;
 
-import net.minecraft.server.v1_4_R1.Item;
-import net.minecraft.server.v1_4_R1.PotionBrewer;
-import net.minecraft.server.v1_4_R1.ItemStack;
+import net.minecraft.server.v1_5_R2.Item;
+import net.minecraft.server.v1_5_R2.PotionBrewer;
+import net.minecraft.server.v1_5_R2.ItemStack;
 
 public class NMSMethods {
 	
@@ -24,6 +24,7 @@ public class NMSMethods {
 	}
 	
 	private static int getBrewResult(int i, ItemStack itemstack) {
-        return itemstack == null ? i : (Item.byId[itemstack.id].v() ? PotionBrewer.a(i, Item.byId[itemstack.id].u()) : i);
+		Item test;
+        return itemstack == null ? i : (Item.byId[itemstack.id].w() ? PotionBrewer.a(i, Item.byId[itemstack.id].v()) : i);
     }
 }
